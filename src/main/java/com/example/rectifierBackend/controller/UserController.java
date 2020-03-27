67fc2c7e7861bbbf3a0a322f68dc539dad1214c7
Michał Górski <mgorski.08@ittcertex.pl>
2020-03-27 20:45:11 +0100

    Database fix

--- src/main/java/com/example/rectifierBackend/controller/UserController.java
+++ src/main/java/com/example/rectifierBackend/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RestController
 public class UserController {
     @Autowired
