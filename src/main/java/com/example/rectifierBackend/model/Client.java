ae1fa666d7a676bd354fa2385e27af20c6412636
Michał Górski <mgorski.08@ittcertex.pl>
2020-05-03 20:31:30 +0200

    Added additional fields to Client

--- src/main/java/com/example/rectifierBackend/model/Client.java
+++ src/main/java/com/example/rectifierBackend/model/Client.java
@@ -15,6 +15,8 @@ public class Client {
     private String address;
     private String city;
     private String zipCode;
+    private String phoneNumber;
+    private String email;
 
     public long getId() {
         return id;
@@ -62,5 +64,21 @@ public class Client {
 
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
